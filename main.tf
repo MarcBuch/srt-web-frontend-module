fdbe2696f64d7b8f76b2710f22fa7283074dbf1e
Marc Buchardt <marc@buchardtprivat.de>
2021-08-01 16:45:13 +0200

    Feature - Transitioned VM to VM scale set

--- main.tf
+++ main.tf
@@ -10,15 +10,23 @@ resource "azurerm_network_interface" "this" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "this" {
-  name                = "${var.prefix}-vm00"
+resource "azurerm_linux_virtual_machine_scale_set" "this" {
+  name                = "${var.prefix}-vmss"
   resource_group_name = var.resource_group_name
   location            = var.region
-  size                = var.vm_size
+  sku                 = var.vm_size
+  instances           = 2
   admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.this.id
-  ]
+
+  network_interface {
+    name    = "${var.prefix}-vmss-nic"
+    primary = true
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = var.subnet_id
+    }
+  }
 
   admin_ssh_key {
     username   = "adminuser"
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "virtual_machine0" {
-  description = "VM"
-  value       = azurerm_linux_virtual_machine.this
+output "vm_scale_set" {
+  description = "VM scale set"
+  value       = azurerm_linux_virtual_machine_scale_set.this
 }
